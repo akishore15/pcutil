53a6f96975af6f315c636cfe37ac48dc3b9c635a
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2026-06-21 19:58:01 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-import subprocess
-import os
-def findgpu():
-  gpu_command = "wmic path win32_VideoController get name"
-  gpu_name = subprocess.check_output(gpu_command, shell=True).decode().strip()
-  if ("AMD" or "Radeon") in gpu_name:
-    os.system('winget install "AMD Software" --source msstore')
-    
